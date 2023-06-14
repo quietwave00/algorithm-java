f2a076cfc4c927f10f71d9a2dae72ab7e270ff0d
갸갸 <quietwave00@gmail.com>
2023-06-14 21:47:27 +0900

    add: 1316

---
+++ basic/P1316_그룹단어체커.java
@@ -0,0 +1,42 @@
+package org.example.basic;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class P1316_그룹단어체커 {
+
+    public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		int result = 0;
+		for(int i = 0; i < N; i++) {
+			String str = br.readLine();
+			if(checkGroupWord(str)) {
+				result++;
+			}
+		}
+		System.out.println(result);
+	}
+
+	private static boolean checkGroupWord(String str) {
+		boolean[] checkArray = new boolean[26];
+		char preStr = 0;
+		for(int i = 0; i < str.length(); i++) {
+			char c = str.charAt(i);
+			if(preStr != c) {
+				if(!checkArray[c - 'a']) {
+					checkArray[c - 'a'] = true;
+					preStr = c;
+				} else {
+					return false;
+				}
+			} else {
+				continue;
+			}
+		}
+		return true;
+	}
+
+}
